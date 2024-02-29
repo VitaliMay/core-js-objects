99b5a620a71adbe28cb66e830518a7b9b93bccc5
VitaliMay <mvt30@mail.ru>
2024-02-29 14:31:10 +0300

    feat: task 03 removeProperties

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -61,8 +61,20 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const newObj = { ...obj };
+  let newKeys = keys;
+  if (!Array.isArray(keys)) {
+    newKeys = [keys];
+  }
+
+  newKeys.forEach((key) => {
+    if (key in newObj) {
+      delete newObj[key];
+    }
+  });
+
+  return newObj;
 }
 
 /**
